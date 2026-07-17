93c1363d1f4baaf24a137d52e87800fe8c551e14
Ji Jetu <jijetu2@gmail.com>
2026-07-17 15:13:55 +0600

    updated

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,5 +1,5 @@
-const serverless = require('serverless-http');
-const app = require('../../rana_server/index.js');
+const serverless = require("serverless-http");
+const app = require("../../rana_server/index.js");
 
 // Wrap the Express app for Netlify
 module.exports.handler = serverless(app, { basePath: '/api' });
